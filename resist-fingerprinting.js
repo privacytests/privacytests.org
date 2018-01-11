d04e30956abc76241df17836db6c3e7dd737df65
Arthur Edelstein <arthuredelstein@gmail.com>
2018-01-10 16:29:38 -0800

    separate out definers

--- resist-fingerprinting.js
+++ resist-fingerprinting.js
@@ -16,41 +16,35 @@ try {
   // Prevent use of arguments.caller.callee.arguments
   "use strict";
 
-// __defineProperties(obj, m)__.
-// Takes an object 'obj' and a map 'm'. The map
-// should have one or more of the keys
-// 'constants', 'getters', 'setAndGetters', 'mutables'.
-const defineProperties = function (obj, m) {
-  if (m.constants) {
-    for (let prop in m.constants) {
-      Object.defineProperty(obj, prop, {
-        value: m.constants[prop],
-        writable: false,
-      })
-    }
+const defineConstants = function (obj, m) {
+  for (let prop in m) {
+    Object.defineProperty(obj, prop, {
+      value: m[prop],
+      writable: false,
+    })
   }
-  if (m.getters) {
-    for (let prop in m.getters) {
-      Object.defineProperty(obj, prop, {
-        get: m.getters[prop],
-      })
-    }
+};
+const defineGetters = function (obj, m) {
+  for (let prop in m) {
+    Object.defineProperty(obj, prop, {
+      get: m[prop],
+    })
   }
-  if (m.settersAndGetters) {
-    for (let {setter, getter} in m.settersAndGetters) {
-      Object.defineProperty(obj, prop, {
-        get: getter,
-        set: setter,
-      })
-    }
+};
+const defineSettersAndGetters = function (obj, m) {
+  for (let {setter, getter} in m) {
+    Object.defineProperty(obj, prop, {
+      get: getter,
+      set: setter,
+    })
   }
-  if (m.mutables) {
-    for (let prop in m.mutables) {
-      Object.defineProperty(obj, prop, {
-        value: m.mutables[prop],
-        writable: true,
-      })
-    }
+};
+const defineMutables = function (obj, m) {
+  for (let prop in m) {
+    Object.defineProperty(obj, prop, {
+      value: m[prop],
+      writable: true,
+    })
   }
 };
 
@@ -59,83 +53,69 @@ const roundTimeMs = t => Math.round(t / 100) * 100;
 // ## rounding performance.now()
 
 const oldNow = Performance.prototype.now;
-defineProperties(Performance.prototype, {
-  constants: {
-    now: function () { return roundTimeMs(oldNow.apply(this)); },
-  },
+defineConstants(Performance.prototype, {
+  now: function () { return roundTimeMs(oldNow.apply(this)); },
 });
 
 // ## window.screen properties
 
-defineProperties(Screen.prototype, {
-  constants: {
-    availLeft: 0,
-    availTop: 0,
-    colorDepth: 24,
-    left: 0,
-    mozOrientation: "landscape-primary",
-    top: 0,
-  },
-  getters: {
-    availHeight: () => window.innerHeight,
-    availWidth: () => window.innerWidth,
-    height: () => window.innerHeight,
-    width: () => window.innerWidth,
-  },
-  mutables: {
-    onmozorientationchange: null, // Does this work?
-  },
+defineConstants(Screen.prototype, {
+  availLeft: 0,
+  availTop: 0,
+  colorDepth: 24,
+  left: 0,
+  mozOrientation: "landscape-primary",
+  top: 0,
+});
+defineGetters(Screen.prototype, {
+  availHeight: () => window.innerHeight,
+  availWidth: () => window.innerWidth,
+  height: () => window.innerHeight,
+  width: () => window.innerWidth,
+});
+defineMutables(Screen.prototype, {
+  onmozorientationchange: null, // Does this work?
 });
 
 // ## screen orientation
 
 if (window.ScreenOrientation) {
-  defineProperties(ScreenOrientation.prototype, {
-    constants: {
-      type: "landscape-primary",
-      angle: 0,
-      onchange: null,
-    },
+  defineConstants(ScreenOrientation.prototype, {
+    type: "landscape-primary",
+    angle: 0,
+    onchange: null,
   });
 };
 
 // ## window.screenX, window.screenY
 
-defineProperties(window, { // Can we use a prototype here instead?
-  constants: {
-    screenX: 0,
-    screenY: 0,
-  },
+defineConstants(window, { // Can we use a prototype here instead?
+  screenX: 0,
+  screenY: 0,
 });
 
 // ## Event.timeStamp
 
 const oldTimeStamp = Object.getOwnPropertyDescriptor(Event.prototype, "timeStamp").get;
 const newTimeStamp = that => roundTimeMs(oldTimeStamp.apply(that));
-defineProperties(Event.prototype, {
-  getters: {
-    timeStamp: function () { return newTimeStamp(this); },
-  }
+defineGetters(Event.prototype, {
+  timeStamp: function () { return newTimeStamp(this); },
 });
 
 // ## MouseEvent.screenX, MouseEvent.screenY
 
-defineProperties(MouseEvent.prototype, {
-  getters: {
-    screenX: function () { return this.clientX; },
-    screenY: function () { return this.clientY; },
-  }
+defineGetters(MouseEvent.prototype, {
+  screenX: function () { return this.clientX; },
+  screenY: function () { return this.clientY; },
 });
 
 // ## window.navigator properties
 
-defineProperties(Navigator.prototype, {
-  constants: {
-    getBattery: undefined,
-    hardwareConcurrency: 2,
-    language: "en-US",
-    languages: "en-US,en",
-  }
+defineConstants(Navigator.prototype, {
+  getBattery: undefined,
+  hardwareConcurrency: 2,
+  language: "en-US",
+  languages: "en-US,en",
 });
 
 // #### Canvas fingerprinting
@@ -162,11 +142,9 @@ const oldToDataURL = HTMLCanvasElement.prototype.toDataURL;
 const newToDataURL = function (canvas, ...args) {
   return oldToDataURL.apply(controlCanvas(canvas), args);
 };
-defineProperties(HTMLCanvasElement.prototype, {
-  constants: {
+defineConstants(HTMLCanvasElement.prototype, {
     toBlob : function (...args) { return newToBlob(this, ...args); },
     toDataURL : function (...args) { return newToDataURL(this, ...args); },
-  }
 });
 
 // ## Date (enforce UTC)
@@ -180,8 +158,8 @@ for (let unit of ["Date", "Day", "FullYear", "Hours", "Milliseconds",
     };
   }
 }
-defineProperties(Date.prototype, {
-    constants: constantMap
+defineConstants(Date.prototype, {
+  constants: constantMap
 });
 
 } catch (e) {
