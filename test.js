bb5018c66ba6adc20e7e2ed679db6a9fe322b402
Arthur Edelstein <arthuredelstein@gmail.com>
2018-01-14 23:16:26 -0800

    transitioning tests to more concise and explicit

--- test.js
+++ test.js
@@ -1,3 +1,15 @@
+// TODO: Let's do the following:
+// 1. Write each test such that the test can be printed in a string, which
+//    is then evaluated and checked against a predicted value.
+// 2. Have a single runAllTests() function that returns the results in JSON.
+// 3. Create a separate script that takes the results and
+//    displays them on test.html or test_browser.html.
+//
+//  The tests can be written first, such that they define the
+//  fingerprinting vectors we are trying to mitigate. "TDD". The tests
+//  can be useful for browser makers to patch their browsers. Also
+//  we can potentially make an automated scorecard using browserstack.com.
+
 const testResultsDiv = document.getElementById("test_results");
 
 const is = function(a, b, description) {
@@ -23,12 +35,21 @@ const test_performance = function () {
   is_rounded_time(now2, "performance.now() via prototype is rounded to nearest 100 ms.");
 };
 
+
+const window_property_tests = [
+  [`screenX`, 0],
+  [`screenY`, 0],
+  [`outerWidth`, `innerWidth`],
+  [`outerHeight`, `innerHeight`],
+  [`devicePixelRatio`, 1]
+];
+
 const test_window_properties = function () {
   is(window.screenX, 0, "screenX is 0.");
   is(window.screenY, 0, "screenY is 0.");
   is(window.outerWidth, window.innerWidth,
      "window.outerWidth spoofed to innerWidth");
-  is(window.outerHeight, window.outerHeight,
+  is(window.outerHeight, window.innerHeight,
      "window.outerHeight spoofed to innerHeight");
   is(window.devicePixelRatio, 1, "devicePixelRatio spoofed to 1");
 };
@@ -39,6 +60,58 @@ const test_mouse_event = function () {
   is(event.screenX, clientX, "MouseEvent.screenX matches .clientX");
   is(event.screenY, clientY, "MouseEvent.screenY matches .clientY");
   is_rounded_time(event.timeStamp, "MouseEvent.timeStamp is rounded");
+};
+
+const screen_tests = [
+  [`screen.width`, `innerWidth`],
+  [`screen.height`, `innerHeight`],
+  [`Screen.prototype.__lookupGetter__("width").apply(screen)`,
+   `innerWidth`],
+  [`Screen.prototype.__lookupGetter__("height").apply(screen)`,
+   `innerHeight`],
+];
+
+
+const navigator_tests = [
+  [`navigator.buildID`, `"20100101"`],
+  [`navigator.getBattery`, undefined],
+  [`try {
+      navigator.getBattery();
+      true;
+    } catch (e) {
+      false;
+   }`,
+   `false`],
+  [`navigator.hardwareConcurrency`, 2],
+  [`Navigator.prototype.__lookupGetter__("hardwareConcurrency")
+   .call(navigator)`, 2],
+  [`navigator.language`, `"en-US"`],
+  [`Navigator.prototype.__lookupGetter__("language").call(navigator)`,
+   `"en-US"`],
+  [`navigator.languages`, `"en-US,en"`],
+  [`Navigator.prototype.__lookupGetter__("languages").call(navigator)`,
+   `"en-US"`],
+  [`navigator.mimeTypes.length`, 0],
+  [`navigator.plugins.length`, 0],
+  [`Object.keys(navigator.mimeTypes).length`, 0],
+  [`Object.keys(navigator.plugins).length`, 0],
+  [`navigator.mimeTypes[0]`, undefined],
+  [`navigator.plugins[0]`, undefined],
+  [`Navigator.prototype.__lookupGetter__("plugins").call(navigator).length`,
+   0],
+  [`Navigator.prototype.__lookupGetter__("mimeTypes").call(navigator).length`,
+   0],
+];
+
+const test_pairs = function (pairs) {
+  for (let [expression, desired_result] of pairs) {
+    const pass = eval(expression) === eval(desired_result);
+    if (pass) {
+      console.log(`PASS: ${expression} --> ${desired_result}`);
+    } else {
+      console.log(`FAIL: ${expression} --> ${desired_result}`);
+    }
+  }
 };
 
 const test_navigator = function () {
@@ -67,6 +140,9 @@ const test_mimeTypes = function () {
 };
 
 const run_tests = async function () {
+  test_pairs(window_property_tests);
+  test_pairs(navigator_tests);
+  test_pairs(screen_tests);
   test_performance();
   test_window_properties();
   test_mouse_event();
