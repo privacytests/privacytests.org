f5b2366204a5c72e05c6787c4c1109f92994e206
Arthur Edelstein <arthuredelstein@gmail.com>
2018-01-16 13:52:08 -0800

    progress on tests

--- test.js
+++ test.js
@@ -10,31 +10,6 @@
 //  can be useful for browser makers to patch their browsers. Also
 //  we can potentially make an automated scorecard using browserstack.com.
 
-const testResultsDiv = document.getElementById("test_results");
-
-const is = function(a, b, description) {
-  const div = document.createElement("div");
-  const pass = (a === b);
-  div.innerText = `${pass ? "PASS" : "FAIL"}: ${description} ${JSON.stringify([a, b])}`;
-  div.setAttribute("class", pass ? "test_pass" : "test_fail");
-  if (pass) {
-    testResultsDiv.appendChild(div);
-  } else {
-    testResultsDiv.insertBefore(div, testResultsDiv.childNodes[0]);
-  }
-}
-
-const is_rounded_time = function (t, description) {
-  is(100 * Math.round(t/100), t, description);
-};
-
-const test_performance = function () {
-  const now = performance.now();
-  is_rounded_time(now, "performance.now() is rounded to nearest 100 ms.");
-  const now2 = Performance.prototype.now.apply(performance);
-  is_rounded_time(now2, "performance.now() via prototype is rounded to nearest 100 ms.");
-};
-
 
 const window_property_tests = [
   [`screenX`, 0],
@@ -44,23 +19,29 @@ const window_property_tests = [
   [`devicePixelRatio`, 1]
 ];
 
-const test_window_properties = function () {
-  is(window.screenX, 0, "screenX is 0.");
-  is(window.screenY, 0, "screenY is 0.");
-  is(window.outerWidth, window.innerWidth,
-     "window.outerWidth spoofed to innerWidth");
-  is(window.outerHeight, window.innerHeight,
-     "window.outerHeight spoofed to innerHeight");
-  is(window.devicePixelRatio, 1, "devicePixelRatio spoofed to 1");
-};
-
-const test_mouse_event = function () {
-  const clientX = 10, clientY = 20;
-  const event = new MouseEvent("click", { clientX, clientY });
-  is(event.screenX, clientX, "MouseEvent.screenX matches .clientX");
-  is(event.screenY, clientY, "MouseEvent.screenY matches .clientY");
-  is_rounded_time(event.timeStamp, "MouseEvent.timeStamp is rounded");
-};
+const performance_timing_tests = [
+  [`performance.timing.connectEnd`, 0],
+  [`performance.timing.connectStart`, 0],
+  [`performance.timing.domComplete`, 0],
+  [`performance.timing.domContentLoadedEventEnd`, 0],
+  [`performance.timing.domContentLoadedEventStart`, 0],
+  [`performance.timing.domInteractive`, 0],
+  [`performance.timing.domLoading`, 0],
+  [`performance.timing.domainLookupEnd`, 0],
+  [`performance.timing.domainLookupStart`, 0],
+  [`performance.timing.fetchStart`, 0],
+  [`performance.timing.loadEventEnd`, 0],
+  [`performance.timing.loadEventStart`, 0],
+  [`performance.timing.navigationStart`, 0],
+  [`performance.timing.redirectEnd`, 0],
+  [`performance.timing.redirectStart`, 0],
+  [`performance.timing.requestStart`, 0],
+  [`performance.timing.responseEnd`, 0],
+  [`performance.timing.responseStart`, 0],
+  [`performance.timing.secureConnectionStart`, 0],
+  [`performance.timing.unloadEventEnd`, 0],
+  [`performance.timing.unloadEventStart`, 0],
+];
 
 const screen_tests = [
   [`screen.width`, `innerWidth`],
@@ -88,9 +69,10 @@ const navigator_tests = [
   [`navigator.language`, `"en-US"`],
   [`Navigator.prototype.__lookupGetter__("language").call(navigator)`,
    `"en-US"`],
-  [`navigator.languages`, `"en-US,en"`],
-  [`Navigator.prototype.__lookupGetter__("languages").call(navigator)`,
-   `"en-US"`],
+  [`navigator.languages.toString()`, `"en-US,en"`],
+  [`Navigator.prototype.__lookupGetter__("languages")
+   .call(navigator).toString()`,
+   `"en-US,en"`],
   [`navigator.mimeTypes.length`, 0],
   [`navigator.plugins.length`, 0],
   [`Object.keys(navigator.mimeTypes).length`, 0],
@@ -103,15 +85,48 @@ const navigator_tests = [
    0],
 ];
 
+let date = new Date();
+
+const date_tests = [
+  [`date.getDate()`, `date.getUTCDate()`],
+];
+
 const test_pairs = function (pairs) {
   for (let [expression, desired_result] of pairs) {
-    const pass = eval(expression) === eval(desired_result);
+    const expression_value = eval(expression);
+    const desired_value = eval(desired_result);
+    const pass = expression_value === desired_value;
     if (pass) {
       console.log(`PASS: ${expression} --> ${desired_result}`);
     } else {
-      console.log(`FAIL: ${expression} --> ${desired_result}`);
+      console.log(`FAIL: ${expression} [${expression_value}] is not ${desired_result} [${desired_value}]`);
     }
   }
+};
+
+const test_performance = function () {
+  const now = performance.now();
+  is_rounded_time(now, "performance.now() is rounded to nearest 100 ms.");
+  const now2 = Performance.prototype.now.apply(performance);
+  is_rounded_time(now2, "performance.now() via prototype is rounded to nearest 100 ms.");
+};
+
+const test_window_properties = function () {
+  is(window.screenX, 0, "screenX is 0.");
+  is(window.screenY, 0, "screenY is 0.");
+  is(window.outerWidth, window.innerWidth,
+     "window.outerWidth spoofed to innerWidth");
+  is(window.outerHeight, window.innerHeight,
+     "window.outerHeight spoofed to innerHeight");
+  is(window.devicePixelRatio, 1, "devicePixelRatio spoofed to 1");
+};
+
+const test_mouse_event = function () {
+  const clientX = 10, clientY = 20;
+  const event = new MouseEvent("click", { clientX, clientY });
+  is(event.screenX, clientX, "MouseEvent.screenX matches .clientX");
+  is(event.screenY, clientY, "MouseEvent.screenY matches .clientY");
+  is_rounded_time(event.timeStamp, "MouseEvent.timeStamp is rounded");
 };
 
 const test_navigator = function () {
@@ -139,16 +154,38 @@ const test_mimeTypes = function () {
      "no element found at index 0 of plugins");
 };
 
+const testResultsDiv = document.getElementById("test_results");
+
+const is = function(a, b, description) {
+  const div = document.createElement("div");
+  const pass = (a === b);
+  div.innerText = `${pass ? "PASS" : "FAIL"}: ${description} ${JSON.stringify([a, b])}`;
+  div.setAttribute("class", pass ? "test_pass" : "test_fail");
+  if (pass) {
+    testResultsDiv.appendChild(div);
+  } else {
+    testResultsDiv.insertBefore(div, testResultsDiv.childNodes[0]);
+  }
+}
+
+const is_rounded_time = function (t, description) {
+  is(100 * Math.round(t/100), t, description);
+};
+
 const run_tests = async function () {
   test_pairs(window_property_tests);
   test_pairs(navigator_tests);
   test_pairs(screen_tests);
+  test_pairs(performance_timing_tests);
+  test_pairs(date_tests);
   test_performance();
   test_window_properties();
   test_mouse_event();
   test_navigator();
   test_mimeTypes();
 };
+
+
 
 run_tests();
 
