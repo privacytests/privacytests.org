332397059fcc2ad1d06d4f83cb52ec3661a9844d
Arthur Edelstein <arthuredelstein@gmail.com>
2018-07-04 09:45:18 -0700

    Simplify further

--- selenium/index.js
+++ selenium/index.js
@@ -8,9 +8,8 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
     await driver.get('https://arthuredelstein.github.io/resist-fingerprinting-js/test_unprotected.html');
 //    await driver.get('file:///home/arthur/resist-fingerprinting-js/test_unprotected.html');
     let body = await driver.findElement(By.tagName('body'));
-    let testResultsString = await driver.wait(async () => {
-      return await body.getAttribute("data-test-results");
-    });
+    let testResultsString = await driver.wait(
+      async () => body.getAttribute("data-test-results"));
     let testResultsObject = JSON.parse(testResultsString);
     console.log(testResultsObject);
     console.log(`${testResultsObject.length} items found.`);
