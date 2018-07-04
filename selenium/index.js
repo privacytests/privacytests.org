19ba23937e3ca4141ccc3c53db96620e1cbc7554
Arthur Edelstein <arthuredelstein@gmail.com>
2018-07-03 20:47:52 -0700

    add selenium test files

---
+++ selenium/index.js
@@ -0,0 +1,21 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+(async function example() {
+  let driver = await new Builder()
+      .forBrowser('firefox')
+      .build();
+  try {
+    await driver.get('https://arthuredelstein.github.io/resist-fingerprinting-js/test_unprotected.html');
+//    await driver.get('file:///home/arthur/resist-fingerprinting-js/test_unprotected.html');
+    let body = await driver.findElement(By.tagName('body'));
+    await driver.wait(async () => {
+      return null !== await body.getAttribute("data-test-results");
+    });
+    let testResultsString = await body.getAttribute("data-test-results");
+    let testResultsObject = JSON.parse(testResultsString);
+    console.log(testResultsObject);
+    console.log(`${testResultsObject.length} items found.`);
+  } finally {
+    await driver.quit();
+  }
+})();
