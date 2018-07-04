57d8ca68208e76059854c03f2dcd90fbb3e672f2
Arthur Edelstein <arthuredelstein@gmail.com>
2018-07-04 09:40:51 -0700

    Minimize attribute accesses

--- selenium/index.js
+++ selenium/index.js
@@ -8,10 +8,9 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
     await driver.get('https://arthuredelstein.github.io/resist-fingerprinting-js/test_unprotected.html');
 //    await driver.get('file:///home/arthur/resist-fingerprinting-js/test_unprotected.html');
     let body = await driver.findElement(By.tagName('body'));
-    await driver.wait(async () => {
-      return null !== await body.getAttribute("data-test-results");
+    let testResultsString = await driver.wait(async () => {
+      return await body.getAttribute("data-test-results");
     });
-    let testResultsString = await body.getAttribute("data-test-results");
     let testResultsObject = JSON.parse(testResultsString);
     console.log(testResultsObject);
     console.log(`${testResultsObject.length} items found.`);
