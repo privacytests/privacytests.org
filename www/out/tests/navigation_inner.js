d2cdad6fa31b794f28c06f4ad7db31505f655bb8
Arthur Edelstein <arthuredelstein@gmail.com>
2020-05-13 18:23:22 -0700

    navigation test files

---
+++ www/out/tests/navigation_inner.js
@@ -0,0 +1,12 @@
+import { runAllTests } from "./test_utils.js";
+
+let tests = {
+  "sessionStorage": {
+    write: (secret) => sessionStorage.setItem("secret", secret),
+    read: () => sessionStorage.getItem("secret"),
+  }
+};
+
+runAllTests(tests);
+
+console.log("hello from navigation_inner.js");
---
+++ www/out/tests/test_utils.js
@@ -0,0 +1,33 @@
+let runTests = async (tests, mode, params) => {
+  let results = {};
+  for (let test of Object.keys(tests)) {
+    let result;
+    try {
+      let input = params[test] || params["default"];
+      console.log("input", input);
+      result = await tests[test][mode](input);
+    } catch (e) {
+      result = "Error: " + e.message;
+    }
+    results[test] = {
+      write: tests[test].write.toString(),
+      read: tests[test].read.toString(),
+      result,
+    };
+  }
+  return results;
+};
+
+let queryParams = (urlString) => {
+  let searchParams = new URL(urlString).searchParams;
+  return Object.fromEntries(searchParams.entries());
+};
+
+export let runAllTests = async (tests) => {
+  let params = queryParams(document.URL);
+  let results = await runTests(tests, params["mode"], params);
+  console.log("results", results);
+  if (window.location !== parent.location) {
+    parent.postMessage(results, "*");
+  }
+};
