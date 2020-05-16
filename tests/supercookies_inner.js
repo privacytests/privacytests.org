b7727af8c4a44508c0e4e5e415743fa7b24485aa
Arthur Edelstein <arthuredelstein@gmail.com>
2020-05-15 23:46:05 -0700

    Updates

--- tests/supercookies_inner.js
+++ tests/supercookies_inner.js
@@ -198,14 +198,14 @@ let tests = {
     write: (key) => new Promise((resolve, reject) => {
       let video = document.createElement("video");
       document.body.appendChild(video);
-      video.addEventListener("loadstart", () => resolve(key), {once: true});
+      video.addEventListener("canplaythrough", () => resolve(key), {once: true});
       video.src = testURI("resource", "video", key);
     }),
     read: async (key) => {
       let video = document.createElement("video");
       document.body.appendChild(video);
       let videoLoadPromise = new Promise((resolve, reject) => {
-        video.addEventListener("loadstart", resolve, {once: true});
+        video.addEventListener("canplaythrough", resolve, {once: true});
       });
       video.src = testURI("resource", "video", key);
       await videoLoadPromise;
