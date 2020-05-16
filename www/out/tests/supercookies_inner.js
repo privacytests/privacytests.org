4088501dfbd5edbd19d88c7f056540889e614eb1
Arthur Edelstein <arthuredelstein@gmail.com>
2020-05-15 23:06:04 -0700

    cleanup

--- www/out/tests/supercookies_inner.js
+++ www/out/tests/supercookies_inner.js
@@ -188,13 +188,7 @@ let tests = {
       let style = document.createElement("style");
       style.type='text/css';
       let fontURI = testURI("resource", "font", key);
-      style.innerHTML = `
-        @font-face {
-          font-family: "myFont";
-          src: url("${fontURI}");
-        }
-        body { font-family: "myFont" }
-      `;
+      style.innerHTML = `@font-face {font-family: "myFont"; src: url("${fontURI}"); } body { font-family: "myFont" }`;
       document.getElementsByTagName("head")[0].appendChild(style);
       let response = await fetch(
         testURI("count", "font", key), {"cache": "reload"});
