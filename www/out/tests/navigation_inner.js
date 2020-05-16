687b7d334c9ae0fb202ccc0531f4112c4af1989a
Arthur Edelstein <arthuredelstein@gmail.com>
2020-05-15 21:19:57 -0700

    add window.name tests

--- www/out/tests/navigation_inner.js
+++ www/out/tests/navigation_inner.js
@@ -4,7 +4,16 @@ let tests = {
   "sessionStorage": {
     write: (secret) => sessionStorage.setItem("secret", secret),
     read: () => sessionStorage.getItem("secret"),
-  }
+  },
+  "window.name": {
+    write: (secret) => parent.postMessage({"write window.name": secret}),
+    read: () => new Promise((resolve) => {
+      parent.postMessage({"read window.name": true}, "*")
+      addEventListener("message", ({data}) => {
+        resolve(data);
+      }, { once: true });
+    })
+  },
 };
 
 runAllTests(tests);
