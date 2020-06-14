7bbd4e74f65f664cc17703fefc8cccb6ed22bfd9
Arthur Edelstein <arthuredelstein@gmail.com>
2020-06-14 08:44:47 -0700

    Updates

--- tests/supercookies_inner.js
+++ tests/supercookies_inner.js
@@ -9,7 +9,7 @@ let sleepMs = (timeMs) => new Promise(
 );
 
 let tests = {
-/*  "cookie": {
+  "cookie": {
     write: (secret) => {
       let expiry = new Date();
       expiry.setFullYear(expiry.getFullYear() + 1);
@@ -69,7 +69,7 @@ let tests = {
         bc.postMessage("request");
         setTimeout(() => reject({message: "no BroadcastChannel message"}), 3000);
       })
-  },*/
+  },
   "fetch": {
     write: async (key) => {
       let response = await fetch(testURI("resource", "fetch", key),
@@ -83,7 +83,7 @@ let tests = {
                                       {cache: "reload"})
       return (await countResponse.text()).trim();
     }
-  },/*
+  },
   "XMLHttpRequest": {
     write: () => new Promise((resolve, reject) => {
       let xhr = new XMLHttpRequest();
@@ -103,7 +103,7 @@ let tests = {
       xhr.send();
       setTimeout(() => reject({message: "XHR: no response"}), 3000);
     })
-  },*/
+  },
   "iframe": {
     write: (key) => new Promise((resolve, reject) => {
       let iframe = document.createElement("iframe");
@@ -124,7 +124,7 @@ let tests = {
         testURI("count", "page", key), {"cache": "reload"});
       return (await response.text()).trim();
     }
-  },/**/
+  },
   "image": {
     write: (key) => new Promise((resolve, reject) => {
       let img = document.createElement("img");
@@ -157,7 +157,7 @@ let tests = {
       return (new URL(url)).searchParams.get("key");
     }
   },
-/*  "favicon": {
+  "favicon": {
     write: (key) => {
       parent.postMessage({
         faviconURI: testURI("resource", "favicon", key)
@@ -193,7 +193,7 @@ let tests = {
         testURI("count", "font", key), {"cache": "reload"});
       return (await response.text()).trim();
     }
-  },*/
+  },
 /*  "video": {
     write: (key) => new Promise((resolve, reject) => {
       let video = document.createElement("video");
@@ -216,7 +216,7 @@ let tests = {
       return (await response.text()).trim();
     }
   },*/
-/*  "locks": {
+  "locks": {
     write: async (key) => {
       if (navigator.locks) {
         navigator.locks.request(key, lock => new Promise((f,r) => {}));
@@ -258,7 +258,7 @@ let tests = {
       image.onerror = () => reject(new Error("image load failed"));
       image.src = "http://hsts.arthuredelstein.net/test_hsts.png";
     })
-  }*/
+  }
 };
 
 runAllTests(tests);
