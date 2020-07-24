0a7ecd51dce4b3287eb324d70f8e214d05921279
Arthur Edelstein <arthuredelstein@gmail.com>
2020-07-24 00:37:37 -0700

    Updates

--- tests/supercookies_inner.js
+++ tests/supercookies_inner.js
@@ -375,10 +375,10 @@ let tests = {
   },
   "h2_connection": {
     write: async (secret) => {
-      await fetch("https://h2.arthuredelstein.net:8901/?mode=write&secret=${secret}");
+      await fetch(`https://h2.arthuredelstein.net:8901/?mode=write&secret=${secret}`);
     },
     read: async () => {
-      let response = await fetch("https://h2.arthuredelstein.net:8901/?mode=read");
+      let response = await fetch(`https://h2.arthuredelstein.net:8901/?mode=read`);
       return await response.text();
     }
   },
