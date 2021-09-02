5d46dafe514385fce329b29a948b8bfcd483f71d
Arthur Edelstein <arthuredelstein@gmail.com>
2021-09-02 12:18:16 -0700

    factor out webdriver_utils.js

--- testing/index.js
+++ testing/index.js
@@ -2,26 +2,16 @@
 
 // ## imports
 
-const homeDir = require('os').homedir();
 const fs = require('fs');
-const {Builder, By, Key, logging, until} = require('selenium-webdriver');
-const firefox = require('selenium-webdriver/firefox');
-const chrome = require('selenium-webdriver/chrome');
-const edge = require('selenium-webdriver/edge');
-const memoize = require('memoizee');
-const fetch = require('node-fetch');
+const {By} = require('selenium-webdriver');
 const dateFormat = require('dateformat');
 const util = require('util');
 const { spawn, exec } = require('child_process');
 const execAsync = util.promisify(exec);
-const { installDriver: installEdgeDriver } = require('ms-chromium-edge-driver');
 const minimist = require('minimist');
 const render = require('./render');
-
+const { createDriver, openNewTab, waitForAttribute } = require('./webdriver_utils.js');
 const DEFAULT_TIMEOUT_MS = 30000;
-
-require('geckodriver');
-require('chromedriver');
 
 // Returns a promise that sleeps for the given millseconds.
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
@@ -29,177 +19,7 @@ const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 // Returns a deep copy of a JSON object.
 const deepCopy = (x) => JSON.parse(JSON.stringify(x));
 
-// ## Selenium setup
-
-// Read a file called .browserstack.json. The file should contain a JSON
-// object that looks like:
-// `
-// {
-//   "user": "my_username",
-//   "key": "my_api_key"
-// }
-// `
-let browserstackCredentials = memoize(
-  () => JSON.parse(fs.readFileSync(homeDir + "/" + ".browserstack.json")),
-  { promise: true });
-
-// Returns a browserstack capabilities object. Required
-// for producing a browserstack driver.
-let fetchBrowserstackCapabilities = async ({user, key}) => {
-  let results = (await fetch(`https://${user}:${key}@api.browserstack.com/automate/browsers.json`)).json();
-//  console.log(await results);
-  return results;
-};
-
-// Takes the long capability list from browserstack.com, and
-// returns all such browsers that match.
-const selectMatchingBrowsers = (allCapabilities, selectionMap) =>
-  allCapabilities.filter((capability) => {
-    let keep = true;
-//    console.log("match", capability, selectionMap);
-    for (let prop in selectionMap) {
-      if (capability[prop] &&
-          selectionMap[prop] &&
-          capability[prop].toLowerCase() !== selectionMap[prop].toLowerCase()) {
-        keep = false;
-      }
-//      console.log(prop, capability[prop], selectionMap[prop], keep);
-      if (keep === false) break;
-    }
-    return keep;
-  });
-
-// Sets Chrome options for the Builder.
-let setChromeOptions = (builder, {incognito, path}) => {
-  let options = new chrome.Options();
-  if (path) {
-    options.setChromeBinaryPath(path);
-  }
-  options.addArguments("--remote-debugging-port=9222");
-  if (incognito) {
-    options.addArguments("incognito");
-  }
-  return builder
-    .setChromeOptions(options)
-    .forBrowser("chrome");
-};
-
-// Sets Edge options for the Builder.
-let setEdgeOptions = async (builder, {incognito, path, local}) => {
-  let options = new edge.Options();
-  if (path) {
-    options.setBinaryPath(path);
-  }
-  if (incognito) {
-    options.addArguments("incognito");
-    options.addArguments("-inprivate");
-  }
-  if (local) {
-    const edgePaths = await installEdgeDriver();
-    //options.setEdgeChromium(true);
-    builder.setEdgeService(new edge.ServiceBuilder(edgePaths.driverPath));
-  }
-  return builder
-    .setEdgeOptions(options)
-    .forBrowser("edge");
-};
-
-// Set Firefox options for the Builder.
-let setFirefoxOptions = (builder, {incognito, path, tor}) => {
-  if (!path && tor) {
-    throw new Error("Please specify a path for the Tor Browser firefox binary.")
-  }
-  let options = new firefox.Options();
-  if (path) {
-    options.setBinary(path);
-  }
-  if (incognito) {
-    options.addArguments("-private");
-  }
-  if (tor) {
-    options.setPreference("extensions.torlauncher.prompt_at_startup", false);
-    options.setPreference("extensions.torlauncher.quickstart", true);
-  }
-  return builder
-    .setFirefoxOptions(options)
-    .forBrowser("firefox")
-};
-
-// Set Safari options for the Builder.
-let setSafariOptions = (builder, {incognito, path}) => {
-  if (incognito) {
-    throw new Error("I don't know how to set incognito mode for Safari.");
-  }
-  if (path) {
-    throw new Error("I don't know how to set a path for Safari.");
-  }
-  return builder.forBrowser("safari");
-};
-
-// Find the best browserstack capabilities that match the specified
-// browser, browser_version, os, and os_version.
-let getBestBrowserstackCapabilities =
-  async ({ user, key, browser, browser_version, os, os_version }) => {
-      let browserstackCapabilities = await fetchBrowserstackCapabilities({user, key});
-    console.log(JSON.stringify([...new Set(browserstackCapabilities.map(x => x["browser"]))], null, "  "));
-  let capabilitiesList = selectMatchingBrowsers(
-    browserstackCapabilities, { browser, os, browser_version, os_version });
-  return capabilitiesList[0];
-};
-
-// Takes the given Builder and sets it up for the specified
-// browser, browser_version, os, and os_version on browserstack.
-let setToBrowserstack =
-    async (builder, { browser, browser_version, os, os_version }) => {
-  let { user, key } = await browserstackCredentials();
-  builder.usingServer(`http://${user}:${key}@hub-cloud.browserstack.com/wd/hub`);
-  let capabilities = await getBestBrowserstackCapabilities(
-    { user, key, browser, browser_version, os, os_version });
-  builder.withCapabilities(capabilities);
-};
-
-// Produces a selenium driver to run tests,
-// using the given config object.
-let createDriver = async ({browser, browser_version,
-                           os, os_version,
-                           service, incognito, path}) => {
-  let builder = new Builder();
-  let browserstack = service === "browserstack";
-    if (browserstack) {
-    await setToBrowserstack(builder, { browser, browser_version, os, os_version });
-  }
-  if (browser === "chrome" || browser === "chromium" || browser === "android" || browser === "samsung" || browser === "opera" || browser === "brave") {
-    setChromeOptions(builder, { incognito, path });
-  } else if (browser === "edge") {
-    await setEdgeOptions(builder, { incognito, path, local: !browserstack });
-  } else if (browser === "firefox" || browser === "tor browser") {
-    setFirefoxOptions(builder, { incognito, path, tor: browser === "tor browser" });
-  } else if (browser === "safari") {
-    setSafariOptions(builder, { incognito, path });
-  } else {
-    throw new Error("unknown browser");
-  }
-  return builder.build();
-};
-
 // ## Testing
-
-// Tell the selenium driver to look at a particular elements's
-// attribute and wait for it to have a value. Returns a promise.
-let waitForAttribute = (driver, element, attrName, timeout) =>
-    driver.wait(async () => element.getAttribute(attrName), timeout);
-
-let openNewTab = async (driver) => {
-  let tabsBefore = await driver.getAllWindowHandles();
-  await driver.switchTo().window(tabsBefore[0]);
-  await driver.get("https://example.com");
-  await driver.executeScript(`
-    document.body.addEventListener("click", () => window.open("https://example.com", "_blank"));
-  `);
-  await driver.findElement(By.tagName('body')).click();
-  let tabsAfter = await driver.getAllWindowHandles();
-  return tabsAfter.filter(x => !tabsBefore.includes(x))[0];
-};
 
 // Tell the selenium driver to visit a url, wait for the attribute
 // "data-test-results" to have a value, and resolve that value
@@ -245,8 +65,11 @@ let runSupercookieTests = async (driver, newTabs) => {
   for (let test in readResultsDifferentFirstParty) {
     let { write, read, result: readDifferentFirstParty } = readResultsDifferentFirstParty[test];
     let { result: readSameFirstParty } = readResultsSameFirstParty[test];
-    let testFailed = !readSameFirstParty || readSameFirstParty.startsWith("Error:");
-    let passed = testFailed ? undefined : (readSameFirstParty !== readDifferentFirstParty);
+    let readSameFirstPartyFailedToFetch = readSameFirstParty ? readSameFirstParty.startsWith("Error: Failed to fetch") : false;
+    let readDifferentFirstPartyFailedToFetch = readDifferentFirstParty ? readDifferentFirstParty.startsWith("Error: Failed to fetch") : false;
+    let testFailed = !readSameFirstParty || (readSameFirstParty.startsWith("Error:") && !readSameFirstPartyFailedToFetch);
+let passed = testFailed ? undefined : ((readSameFirstParty !== readDifferentFirstParty) ||
+                                       (readSameFirstPartyFailedToFetch && readDifferentFirstPartyFailedToFetch));
     jointResult[test] = { write, read, readSameFirstParty, readDifferentFirstParty, passed, testFailed };
   }
 //  console.log("readResultsDifferentFirstParty:", readResultsDifferentFirstParty);
---
+++ testing/webdriver_utils.js
@@ -0,0 +1,188 @@
+/* jshint esversion: 6 */
+
+// ## imports
+
+const homeDir = require('os').homedir();
+const fs = require('fs');
+const {Builder, By, Key, logging, until} = require('selenium-webdriver');
+const firefox = require('selenium-webdriver/firefox');
+const chrome = require('selenium-webdriver/chrome');
+const edge = require('selenium-webdriver/edge');
+const memoize = require('memoizee');
+const fetch = require('node-fetch');
+const { installDriver: installEdgeDriver } = require('ms-chromium-edge-driver');
+
+require('geckodriver');
+require('chromedriver');
+
+// Read a file called .browserstack.json. The file should contain a JSON
+// object that looks like:
+// `
+// {
+//   "user": "my_username",
+//   "key": "my_api_key"
+// }
+// `
+let browserstackCredentials = memoize(
+  () => JSON.parse(fs.readFileSync(homeDir + "/" + ".browserstack.json")),
+  { promise: true });
+
+// Returns a browserstack capabilities object. Required
+// for producing a browserstack driver.
+let fetchBrowserstackCapabilities = async ({user, key}) => {
+  let results = (await fetch(`https://${user}:${key}@api.browserstack.com/automate/browsers.json`)).json();
+//  console.log(await results);
+  return results;
+};
+
+// Takes the long capability list from browserstack.com, and
+// returns all such browsers that match.
+const selectMatchingBrowsers = (allCapabilities, selectionMap) =>
+  allCapabilities.filter((capability) => {
+    let keep = true;
+//    console.log("match", capability, selectionMap);
+    for (let prop in selectionMap) {
+      if (capability[prop] &&
+          selectionMap[prop] &&
+          capability[prop].toLowerCase() !== selectionMap[prop].toLowerCase()) {
+        keep = false;
+      }
+//      console.log(prop, capability[prop], selectionMap[prop], keep);
+      if (keep === false) break;
+    }
+    return keep;
+  });
+
+// Sets Chrome options for the Builder.
+let setChromeOptions = (builder, {incognito, path}) => {
+  let options = new chrome.Options();
+  if (path) {
+    options.setChromeBinaryPath(path);
+  }
+  options.addArguments("--remote-debugging-port=9222");
+  if (incognito) {
+    options.addArguments("incognito");
+  }
+  return builder
+    .setChromeOptions(options)
+    .forBrowser("chrome");
+};
+
+// Sets Edge options for the Builder.
+let setEdgeOptions = async (builder, {incognito, path, local}) => {
+  let options = new edge.Options();
+  if (path) {
+    options.setBinaryPath(path);
+  }
+  if (incognito) {
+    options.addArguments("incognito");
+    options.addArguments("-inprivate");
+  }
+  if (local) {
+    const edgePaths = await installEdgeDriver();
+    //options.setEdgeChromium(true);
+    builder.setEdgeService(new edge.ServiceBuilder(edgePaths.driverPath));
+  }
+  return builder
+    .setEdgeOptions(options)
+    .forBrowser("edge");
+};
+
+// Set Firefox options for the Builder.
+let setFirefoxOptions = (builder, {incognito, path, tor}) => {
+  if (!path && tor) {
+    throw new Error("Please specify a path for the Tor Browser firefox binary.")
+  }
+  let options = new firefox.Options();
+  if (path) {
+    options.setBinary(path);
+  }
+  if (incognito) {
+    options.addArguments("-private");
+  }
+  if (tor) {
+    options.setPreference("extensions.torlauncher.prompt_at_startup", false);
+    options.setPreference("extensions.torlauncher.quickstart", true);
+  }
+  return builder
+    .setFirefoxOptions(options)
+    .forBrowser("firefox")
+};
+
+// Set Safari options for the Builder.
+let setSafariOptions = (builder, {incognito, path}) => {
+  if (incognito) {
+    throw new Error("I don't know how to set incognito mode for Safari.");
+  }
+  if (path) {
+    throw new Error("I don't know how to set a path for Safari.");
+  }
+  return builder.forBrowser("safari");
+};
+
+// Find the best browserstack capabilities that match the specified
+// browser, browser_version, os, and os_version.
+let getBestBrowserstackCapabilities =
+  async ({ user, key, browser, browser_version, os, os_version }) => {
+      let browserstackCapabilities = await fetchBrowserstackCapabilities({user, key});
+    console.log(JSON.stringify([...new Set(browserstackCapabilities.map(x => x["browser"]))], null, "  "));
+  let capabilitiesList = selectMatchingBrowsers(
+    browserstackCapabilities, { browser, os, browser_version, os_version });
+  return capabilitiesList[0];
+};
+
+// Takes the given Builder and sets it up for the specified
+// browser, browser_version, os, and os_version on browserstack.
+let setToBrowserstack =
+    async (builder, { browser, browser_version, os, os_version }) => {
+  let { user, key } = await browserstackCredentials();
+  builder.usingServer(`http://${user}:${key}@hub-cloud.browserstack.com/wd/hub`);
+  let capabilities = await getBestBrowserstackCapabilities(
+    { user, key, browser, browser_version, os, os_version });
+  builder.withCapabilities(capabilities);
+};
+
+// Produces a selenium driver to run tests,
+// using the given config object.
+let createDriver = async ({browser, browser_version,
+                           os, os_version,
+                           service, incognito, path}) => {
+  let builder = new Builder();
+  let browserstack = service === "browserstack";
+    if (browserstack) {
+    await setToBrowserstack(builder, { browser, browser_version, os, os_version });
+  }
+  if (browser === "chrome" || browser === "chromium" || browser === "android" || browser === "samsung" || browser === "opera" || browser === "brave") {
+    setChromeOptions(builder, { incognito, path });
+  } else if (browser === "edge") {
+    await setEdgeOptions(builder, { incognito, path, local: !browserstack });
+  } else if (browser === "firefox" || browser === "tor browser") {
+    setFirefoxOptions(builder, { incognito, path, tor: browser === "tor browser" });
+  } else if (browser === "safari") {
+    setSafariOptions(builder, { incognito, path });
+  } else {
+    throw new Error("unknown browser");
+  }
+  return builder.build();
+};
+
+// Tell the selenium driver to look at a particular elements's
+// attribute and wait for it to have a value. Returns a promise.
+let waitForAttribute = (driver, element, attrName, timeout) =>
+    driver.wait(async () => element.getAttribute(attrName), timeout);
+
+// Tell the selenium driver to open a new tab at https://example.com.
+// Returns a promise containing a handle to the new tab window.
+let openNewTab = async (driver) => {
+  let tabsBefore = await driver.getAllWindowHandles();
+  await driver.switchTo().window(tabsBefore[0]);
+  await driver.get("https://example.com");
+  await driver.executeScript(`
+    document.body.addEventListener("click", () => window.open("https://example.com", "_blank"));
+  `);
+  await driver.findElement(By.tagName('body')).click();
+  let tabsAfter = await driver.getAllWindowHandles();
+  return tabsAfter.filter(x => !tabsBefore.includes(x))[0];
+};
+
+module.exports = { createDriver, waitForAttribute, openNewTab };
