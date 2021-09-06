bc6e8e479de216d706ef5bb22d2f82c12fbdb135
Arthur Edelstein <arthuredelstein@gmail.com>
2021-09-06 10:20:53 -0700

    Updates

--- tests/network.js
+++ tests/network.js
@@ -28,6 +28,7 @@ const testDoH = async () => {
   }
   let nextDoH;
   try {
+    // TODO: Get this working correctly.
     let nextDoHResponse = await fetchJSON("https://test.nextdns.io/");
     nextDoH = nextDoHResponse["status"] === "unconfigured";
   } catch (e) {
@@ -37,10 +38,18 @@ const testDoH = async () => {
   return { cloudflareDoH, nextDoH, passed };
 };
 
+const testGPC = async () => {
+  // Ask the server what headers it sees.
+  const requestHeaders = await fetchJSON("https://arthuredelstein.net/browser-privacy-live/headers");
+  const passed = requestHeaders["sec-gpc"] === "1";
+  return { "sec-gpc": requestHeaders["sec-gpc"], passed };
+};
+
 let runTests = async () => {
   let resultsJSON = {
     "Tor enabled": await testTor(),
-    "DoH enabled": await testDoH()
+    "DoH enabled": await testDoH(),
+    "GPC enabled": await testGPC()
   };
   console.log(resultsJSON);
   document.body.setAttribute("data-test-results", JSON.stringify(resultsJSON));
