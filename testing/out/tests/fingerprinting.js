c90bb517f86117c3cf39ac69c3da3f4bf4bfc719
Arthur Edelstein <arthuredelstein@gmail.com>
2021-09-08 14:34:16 -0700

    Remove obsolete fingerprinting tests

--- testing/out/tests/fingerprinting.js
+++ testing/out/tests/fingerprinting.js
@@ -17,45 +17,12 @@ const dual_tests = function dual_tests () {
 const performance_now_tests = [
   [`performance.now()`,
    `Math.floor(performance.now() / 100) * 100`],
-  [`Performance.prototype.now.apply(performance)`,
-   `Math.floor(performance.now() / 100) * 100`],
-];
-
-// ## Date object
-
-const date = new Date();
-const date_tests = [
-  [`date.getDate()`, `date.getUTCDate()`],
-  [`date.getDay()`, `date.getUTCDay()`],
-  [`date.getFullYear()`, `date.getUTCFullYear()`],
-  [`date.getHours()`, `date.getUTCHours()`],
-  [`date.getMilliseconds()`, `date.getUTCMilliseconds()`],
-  [`date.getMinutes()`, `date.getUTCMinutes()`],
-  [`date.getMonth()`, `date.getUTCMonth()`],
-  [`date.getSeconds()`, `date.getUTCSeconds()`],
-  [`Date.prototype.getDate.call(date)`, `date.getUTCDate()`],
-  [`Date.prototype.getDay.call(date)`, `date.getUTCDay()`],
-  [`Date.prototype.getFullYear.call(date)`, `date.getUTCFullYear()`],
-  [`Date.prototype.getHours.call(date)`, `date.getUTCHours()`],
-  [`Date.prototype.getMilliseconds.call(date)`, `date.getUTCMilliseconds()`],
-  [`Date.prototype.getMinutes.call(date)`, `date.getUTCMinutes()`],
-  [`Date.prototype.getMonth.call(date)`, `date.getUTCMonth()`],
-  [`Date.prototype.getSeconds.call(date)`, `date.getUTCSeconds()`],
 ];
 
 // ## Intl object
 
-const intl_tests = [
-  [`Intl.Collator().resolvedOptions().locale`, `"en-US"`],
-  [`Intl.DateTimeFormat().resolvedOptions().locale`, `"en-US"`],
-  [`Intl.NumberFormat().resolvedOptions().locale`, `"en-US"`],
-  [`new Intl.PluralRules().resolvedOptions().locale`, `"en-US"`],
-];
-
 const dual_navigator_tests = [
   [`navigator.hardwareConcurrency`, 2],
-  [`navigator.language`, `"en-US"`],
-  [`navigator.languages.toString()`, `"en-US,en"`],
 ];
 
 const event_tests = [
@@ -86,10 +53,8 @@ const test_pairs = (pairs) => pairs.map(
 
 const run_all_tests = function () {
   return [].concat(
-    test_pairs(intl_tests),
     test_pairs(dual_navigator_tests),
     test_pairs(performance_now_tests),
-    test_pairs(date_tests),
     test_pairs(event_tests),
   );
 };
@@ -111,37 +76,9 @@ const window_property_tests = [
   [`devicePixelRatio`, 1]
 ];
 
-const performance_timing_tests = [
-  [`performance.timing.connectEnd`, 0],
-  [`performance.timing.connectStart`, 0],
-  [`performance.timing.domComplete`, 0],
-  [`performance.timing.domContentLoadedEventEnd`, 0],
-  [`performance.timing.domContentLoadedEventStart`, 0],
-  [`performance.timing.domInteractive`, 0],
-  [`performance.timing.domLoading`, 0],
-  [`performance.timing.domainLookupEnd`, 0],
-  [`performance.timing.domainLookupStart`, 0],
-  [`performance.timing.fetchStart`, 0],
-  [`performance.timing.loadEventEnd`, 0],
-  [`performance.timing.loadEventStart`, 0],
-  [`performance.timing.navigationStart`, 0],
-  [`performance.timing.redirectEnd`, 0],
-  [`performance.timing.redirectStart`, 0],
-  [`performance.timing.requestStart`, 0],
-  [`performance.timing.responseEnd`, 0],
-  [`performance.timing.responseStart`, 0],
-  [`performance.timing.secureConnectionStart`, 0],
-  [`performance.timing.unloadEventEnd`, 0],
-  [`performance.timing.unloadEventStart`, 0],
-];
-
 const screen_tests = [
   [`screen.width`, `innerWidth`],
   [`screen.height`, `innerHeight`],
-  [`Screen.prototype.__lookupGetter__("width").apply(screen)`,
-   `innerWidth`],
-  [`Screen.prototype.__lookupGetter__("height").apply(screen)`,
-   `innerHeight`],
 ];
 
 let navigator_tests = [
@@ -156,23 +93,7 @@ let navigator_tests = [
    `false`],
   [`navigator.mimeTypes.length`, 0],
   [`navigator.plugins.length`, 0],
-  [`Object.getPrototypeOf(navigator.plugins) === PluginArray.prototype`, true],
-  [`Object.getPrototypeOf(navigator.mimeTypes) === MimeTypeArray.prototype`, true],
-  [`Object.keys(navigator.mimeTypes).length`, 0],
-  [`Object.keys(navigator.plugins).length`, 0],
-  [`navigator.mimeTypes[0]`, undefined],
-  [`navigator.plugins[0]`, undefined],
-  [`Navigator.prototype.__lookupGetter__("plugins").call(navigator).length`, 0],
-  [`Navigator.prototype.__lookupGetter__("mimeTypes").call(navigator).length`, 0],
 ];
-
-if (navigator.getGamepads) {
-  navigator_tests.concat([
-    [`navigator.getGamepads().length`, 0],
-    [`Array.isArray(navigator.getGamepads())`, true],
-    [`Navigator.prototype.getGamepads.call(navigator).length`, 0],
-  ]);
-}
 
 const mouse_event_tests = [
   [`mouseEvent.screenX`, `mouseEvent.clientX`],
@@ -212,7 +133,6 @@ const run_all_tests = async function () {
     test_pairs(window_property_tests),
     test_pairs(navigator_tests),
     test_pairs(screen_tests),
-    test_pairs(performance_timing_tests),
     test_pairs(mouse_event_tests),
   );
   return list_to_map(all_tests,
