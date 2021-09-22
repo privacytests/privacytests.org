c0544afb57e289054045feda6dab8033a8efe586
Arthur Edelstein <arthuredelstein@gmail.com>
2021-09-21 18:02:06 -0700

    Updates

--- tests/supercookies_inner.js
+++ tests/supercookies_inner.js
@@ -272,7 +272,7 @@ let tests = {
     write: async (key) => {
       let prime = await fetch(testURI("etag", "", key));
       let response = await fetch(testURI("etag", "", key));
-      let etagHeader = response.headers["etag"];
+      let etagHeader = response.headers.get("etag");
       if (etagHeader === "undefined") {
         return undefined;
       } else {
@@ -281,7 +281,7 @@ let tests = {
     },
     read: async (key) => {
       let response = await fetch(testURI("etag", "", key));
-      let etagHeader = response.headers["etag"];
+      let etagHeader = response.headers.get("etag");
       if (etagHeader === "undefined") {
         return undefined;
       } else {
