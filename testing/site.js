276b26c4253a88dfbdec0ff8ac3ee739c31aafdb
Arthur Edelstein <arthuredelstein@gmail.com>
2021-09-26 12:38:25 -0700

    site.js

---
+++ testing/site.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const marked = require("marked");
+const htmlUtils = require('./html-utils.js');
+
+const wrapCopy = (content) => `
+  <div class="copy">${content}</div>
+`;
+
+const generateFile = (filename) => {
+  const copy = fs.readFileSync(`copy/${filename}`, "utf8");
+  const newFilename = filename.replace(".md", ".html");
+  const htmlOutput = htmlUtils.htmlPage( {
+    title: "Browser Privacy Tests",
+    content: wrapCopy(marked(copy))
+  });
+  console.log(htmlOutput);
+  fs.writeFileSync(`out/${newFilename}`, htmlOutput, "utf8");
+};
+
+const main = async () => {
+  generateFile("about.md");
+};
+
+main();
