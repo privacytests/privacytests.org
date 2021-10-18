8ed19b44641b44c231a18266e344f5f1f80e0a0c
Arthur Edelstein <arthuredelstein@gmail.com>
2021-10-18 19:09:20 +0000

    semicolons

--- live/caching.js
+++ live/caching.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3333
+const express = require('express');
+const app = express();
+const port = 3333;
 
 let countMaps = {
   "css": {},
