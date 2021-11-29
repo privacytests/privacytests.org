6f0584742521e69452edbdc5aacd2e87ed265701
Arthur Edelstein <arthuredelstein@gmail.com>
2021-11-29 12:33:26 -0800

    send category from postData

--- testing/out/tests/post_data.js
+++ testing/out/tests/post_data.js
@@ -1,7 +1,7 @@
 // postResults(results) learns the sessionId for this page load,
 // and sends the results to results.privacytests.org/post under
 // that sessionId.
-const postData = async (results, type) => {
+const postData = async (results, category) => {
   const urlParams = new URLSearchParams(window.location.search);
   if (!urlParams.has("sessionId")) {
     return;
@@ -14,13 +14,13 @@ const postData = async (results, type) => {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({sessionId, url: window.location.href, "data": results, type})
+    body: JSON.stringify({sessionId, url: window.location.href, "data": results, category })
   });
   return await response.json();
 };
 
-const postDataAndCarryOn = async (results, type) => {
-  const { newTabUrl, navigateUrl } = await postData(results, type);
+const postDataAndCarryOn = async (results, category) => {
+  const { newTabUrl, navigateUrl } = await postData(results, category);
   if (newTabUrl) {
     window.open(newTabUrl, "_blank");
   }
