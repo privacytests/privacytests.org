9ae1289e4e784571b0036594c48e0352632ebcad
Arthur Edelstein <arthuredelstein@gmail.com>
2021-11-30 20:48:34 +0000

    always use sessionId as our secret

--- testing/out/tests/serviceWorker.js
+++ testing/out/tests/serviceWorker.js
@@ -9,7 +9,7 @@ self.addEventListener("fetch", async (event) => {
   let shortPath = event.request.url.split(scope)[1];
   if (shortPath) {
     if (shortPath.startsWith("serviceworker-write?")) {
-      secret = (new URL(event.request.url)).searchParams.get("secret");
+      secret = (new URL(event.request.url)).searchParams.get("sessionId");
       event.respondWith(new Response(""));
     } else if (shortPath === "serviceworker-read") {
       event.respondWith(new Response(secret));
