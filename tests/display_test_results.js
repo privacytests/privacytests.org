488ac9e7d3db456d6916fd1b09b2d8c7afa2eccc
Arthur Edelstein <arthuredelstein@gmail.com>
2021-12-15 23:25:19 -0800

    Updates

--- tests/display_test_results.js
+++ tests/display_test_results.js
@@ -1,19 +1,26 @@
 const run_and_display_all_tests = async function (testResultsDiv) {
-  let results = await run_all_tests();
-  let keys = Object.keys(results).sort();
-  console.log(results);
-  document.body.setAttribute("data-test-results", JSON.stringify(results));
-  for (let key of keys) {
-    let { expression, spoof_expression, actual_value, desired_value, passed } = results[key];
-    const div = document.createElement("div");
-    div.innerText = `${passed ? "PASS" : "FAIL"}: ${key} [${actual_value}, ${desired_value}]`;
-    div.setAttribute("class", passed ? "test_pass" : "test_fail");
-    if (passed) {
-      testResultsDiv.appendChild(div);
-    } else {
-      testResultsDiv.insertBefore(div, testResultsDiv.childNodes[0]);
+  let results = {};
+  try {
+    results = await run_all_tests();
+    let keys = Object.keys(results).sort();
+    console.log(results);
+    document.body.setAttribute("data-test-results", JSON.stringify(results));
+    for (let key of keys) {
+      let { expression, spoof_expression, actual_value, desired_value, passed } = results[key];
+      const div = document.createElement("div");
+      div.innerText = `${passed ? "PASS" : "FAIL"}: ${key} [${actual_value}, ${desired_value}]`;
+      div.setAttribute("class", passed ? "test_pass" : "test_fail");
+      if (passed) {
+        testResultsDiv.appendChild(div);
+      } else {
+        testResultsDiv.insertBefore(div, testResultsDiv.childNodes[0]);
+      }
     }
+  } catch (e) {
+    results["error"] = e.toString();
+    document.body.innerHTML += `<div>${e.toString()}</div>`;
   }
+
   await postDataAndCarryOn(results, "fingerprinting");
 };
 
