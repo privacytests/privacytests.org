39b04a49388336498c662f14a4a6354679bc4145
Arthur Edelstein <arthuredelstein@gmail.com>
2022-02-22 18:35:14 -0800

    production-tests.sh

---
+++ testing/production-tests.sh
@@ -0,0 +1,10 @@
+/usr/local/bin/node test config/desktop.yaml --update
+/usr/local/bin/node test config/nightly.yaml --update
+/usr/local/bin/node test config/desktop.yaml >> desktop-log.txt
+/usr/local/bin/node test config/desktop.yaml --incognito --filename private >> private-log.txt
+/usr/local/bin/node test config/nightly.yaml >> nightly-log.txt
+/usr/local/bin/node test config/nightly.yaml --incognito --filename nightly-private >> nightly-private-log.txt
+/usr/local/bin/node test config/android.yaml >> android-log.txt
+/usr/local/bin/node test config/iOS.yaml >> ios-log.txt
+#/usr/local/bin/node test config/android-nightly.yaml >> android-nightly-log.txt
+
