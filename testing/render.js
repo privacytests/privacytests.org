3e5a84ede0a064788e3e7d908ae9266fba56da33
Arthur Edelstein <arthuredelstein@gmail.com>
2022-03-08 18:59:08 -0800

    correct obsolete property

--- testing/render.js
+++ testing/render.js
@@ -24,10 +24,10 @@ const browserLogoDataUri = _.memoize((browserName, nightly) => {
   const browserIconName = nightly ? nightlyIconNames[browserName] : browserName;
   let iconUri;
   try {
-    iconUri = dataUriFromFile(`node_modules/browser-logos/src/${browserIconName}/${browserIconName}_128x128.png`).content;
+    iconUri = dataUriFromFile(`node_modules/browser-logos/src/${browserIconName}/${browserIconName}_128x128.png`);
     return iconUri;
   } catch (e) {
-    return template.dataUriFromFile(`icons/${browserIconName}.png`).content;
+    return template.dataUriFromFile(`icons/${browserIconName}.png`);
   }
 });
 
