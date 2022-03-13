572dc4254aa4705d93f5d59add667dd94cbd7371
Arthur Edelstein <arthuredelstein@gmail.com>
2022-03-13 11:15:04 -0700

    remove empty margins on preview image

--- testing/prepare.mjs
+++ testing/prepare.mjs
@@ -2,8 +2,8 @@ import captureWebsite from 'capture-website';
 
 const createPreviewImage = (htmlFile, pngFile) => {
     captureWebsite.file(htmlFile, pngFile, {
-      width: 1200,
-      height: 627, scaleFactor: 1,
+      width: 1000,
+      height: 500, scaleFactor: 1.2,
       overwrite: true
     });
 }
