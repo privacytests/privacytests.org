be770f12d2ad2e5706ad9f62520eeb8679f8b30a
Arthur Edelstein <arthuredelstein@gmail.com>
2022-04-22 15:23:05 -0700

    node ready YYYYMMDD

--- testing/ready.js
+++ testing/ready.js
@@ -4,7 +4,7 @@ const { execSync } = require('child_process');
 
 // Constants
 const allowedSuffixes = [".html", ".json", ".png"];
-const indexPath = "out/";
+const indexPath = "out";
 
 // Ensure a directory exists.
 const createDir = (path) => {
@@ -19,6 +19,7 @@ const copyDirFiles = (src, dest, suffixes) => {
   for (let file of files) {
     if (suffixes.some(suffix => file.endsWith(suffix))) {
       fs.copyFileSync(`${src}/${file}`, `${dest}/${file}`);
+      console.log(`copied ${src}/${file} to ${dest}/${file}`);
     }
   }
 };
@@ -27,7 +28,7 @@ const copyDirFiles = (src, dest, suffixes) => {
 const copyPublishableFiles = (resultsPath) => {
   const versionNumber = fs.readFileSync("issue-number").toString().trim();
   const archivePath = `out/archive/issue${versionNumber}`;
-  console.log(versionNumber);
+  console.log("version found:", versionNumber);
   createDir(archivePath);
   const files = fs.readdirSync(resultsPath);
   copyDirFiles(resultsPath, archivePath, allowedSuffixes);
@@ -37,13 +38,15 @@ const copyPublishableFiles = (resultsPath) => {
 // Add files in path with given suffixes to git (but don't commit yet)
 const gitAddFiles = (path, suffixes) => {
   const wildcards = ["",...suffixes].join(" *");
-
+  const command = `git add ${wildcards}`;
+  console.log(`In directory ${path}:`, command);
   execSync(`git add ${wildcards}`, {cwd: path});
 };
 
 // The main function. Copy publishable files, and add them to git.
 const main = () => {
-  const resultsPath = "out/results/20220422";
+  const date = process.argv[2]
+  const resultsPath = `out/results/${date}`;
   copyPublishableFiles(resultsPath);
   gitAddFiles(resultsPath, allowedSuffixes);
   gitAddFiles(indexPath, allowedSuffixes);
