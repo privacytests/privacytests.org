e39ddad3531ab4c02ee8bbb3c52b05bc8a20fac7
Arthur Edelstein <arthuredelstein@gmail.com>
2022-05-01 09:53:20 -0700

    log and await preview image prep

--- testing/prepare.mjs
+++ testing/prepare.mjs
@@ -1,11 +1,12 @@
 import captureWebsite from 'capture-website';
 
-const createPreviewImage = (htmlFile, pngFile) => {
-    captureWebsite.file(htmlFile, pngFile, {
+const createPreviewImage = async (htmlFile, pngFile) => {
+    await captureWebsite.file(htmlFile, pngFile, {
       width: 1000,
       height: 500, scaleFactor: 1.2,
       overwrite: true
     });
+    console.log("wrote", pngFile);
 }
 
 export { createPreviewImage };
