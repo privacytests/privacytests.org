77527987672fa447893b55a5c0204bf346a7a20b
Arthur Edelstein <arthuredelstein@gmail.com>
2022-10-01 18:33:17 +0000

    start moving *.arthuredelstein.net subdomains to privacytests2.org

--- static/https.js
+++ static/https.js
@@ -20,7 +20,7 @@ const insecureSubresourceTest = async (tag, fileName) => {
   let fileTypeNames = { "img": "image", "script": "script" };
   const description = `Checks to see if the browser attempts to upgrade an insecure address for an ${fileTypeNames[tag]} to HTTPS whenever possible.`;
   let upgradableEvent = await loadSubresource(tag, `http://upgradable.arthuredelstein.net/content/${fileName}`);
-  let insecureEvent = await loadSubresource(tag, `http://insecure.arthuredelstein.net/content/${fileName}`);
+  let insecureEvent = await loadSubresource(tag, `http://insecure.privacytests2.org/content/${fileName}`);
   let passed = insecureEvent.type === "error";
   let putativeUpgradeHandling = upgradableEvent.type === "load" ? "upgraded" : "blocked";
   let result = passed ? putativeUpgradeHandling : "loaded insecurely";
