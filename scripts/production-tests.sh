c22500cb459d15e90dbeafc38eefad68290b211c
Arthur Edelstein <arthuredelstein@gmail.com>
2022-10-26 10:19:01 -0700

    duckduckgo doesn't have private windows currently

--- scripts/production-tests.sh
+++ scripts/production-tests.sh
@@ -3,7 +3,7 @@
 /usr/local/bin/node test config/desktop.yaml >> desktop-log.txt
 /usr/local/bin/node test config/desktop.yaml --incognito --filename private >> private-log.txt
 /usr/local/bin/node test config/nightly.yaml >> nightly-log.txt
-/usr/local/bin/node test config/nightly.yaml --incognito --filename nightly-private >> nightly-private-log.txt
+/usr/local/bin/node test config/nightly.yaml --incognito --filename nightly-private --except=duckduckgo >> nightly-private-log.txt
 /usr/local/bin/node test config/android.yaml >> android-log.txt
 /usr/local/bin/node test config/iOS.yaml >> ios-log.txt
 #/usr/local/bin/node test config/android-nightly.yaml >> android-nightly-log.txt
