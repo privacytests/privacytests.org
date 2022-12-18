695084f0192210ff21aa1342369917d9a497807b
Arthur Edelstein <arthuredelstein@gmail.com>
2022-12-17 22:07:14 -0800

    toplevel.html suffix and content type

--- scripts/test.js
+++ scripts/test.js
@@ -342,7 +342,7 @@ const runTestsStage1 = async ({browserObject, categories}) => {
   }
   // Misc
   if (!categories || categories.includes("misc")) {
-    const topLevelResults = await runPageTest(browserObject, `${live_root}/toplevel`)
+    const topLevelResults = await runPageTest(browserObject, `${live_root}/toplevel.html`)
     console.log("misc results:", {topLevelResults});
     const ipAddressLeak = await ipAddressTest(topLevelResults);
     Object.assign(results["misc"],
