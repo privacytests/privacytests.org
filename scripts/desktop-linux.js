35d926aae32fc97c8ed8a604a465f1d76b77c46e
Arthur Edelstein <arthuredelstein@gmail.com>
2022-12-19 09:33:27 -0800

    desktop-linux.js comments

--- scripts/desktop-linux.js
+++ scripts/desktop-linux.js
@@ -1,6 +1,5 @@
 const child_process = require('child_process');
 
-_
 const linuxDefaultBrowserSettings = {
   brave: {
     command: "/usr/bin/brave-browser",
@@ -29,16 +28,19 @@ const profileFlags = {
   "safari": undefined,
 };
 
+// Execute a command asynchronously.
 const exec = (command, options) => {
   console.log(command);
   return child_process.exec(command, options);
 };
 
+// Execute a synchronous command.
 const execSync = (command, options) => {
   console.log(command);
   return child_process.exec(command, options).toString();
 };
 
+// Returns an list of the PIDs for immediate children
 const childProcesses = (pid) => {
   try {
     const list = child_process.execSync(`pgrep -P ${pid}`).toString();
@@ -48,16 +50,20 @@ const childProcesses = (pid) => {
   }
 }
 
+// Returns all processes that are descendants of PID, including
+// the original PID itself.
 const descendantProcesses = (pid) => {
   const children = childProcesses(pid);
   descendants = children.map(descendantProcesses).flat();
   return [pid, ...descendants];
 };
 
+// Kill all processes in the list of PIDs.
 const killProcesses = (pids) => {
   pids.map(pid => process.kill(pid, process.SIGTERM));
 };
 
+// Declares a class that represents a browser on Linux.
 class DesktopBrowserLinux {
   constructor({ browser, path, incognito, tor, nightly, appDir }) {
     this._defaults = linuxDefaultBrowserSettings[browser];
