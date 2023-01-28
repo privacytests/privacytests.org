f6d0ecd99fb37c45abd10b242af616c01b215f1a
Arthur Edelstein <arthuredelstein@gmail.com>
2023-01-28 00:31:06 -0800

    issue 42 with news

--- scripts/production-tests.sh
+++ scripts/production-tests.sh
@@ -2,9 +2,9 @@
 #/usr/local/bin/node test config/nightly.yaml --update
 /usr/local/bin/node test config/desktop.yaml >> desktop-log.txt
 /usr/local/bin/node test config/desktop.yaml --incognito --filename private >> private-log.txt
-/usr/local/bin/node test config/nightly.yaml >> nightly-log.txt
-/usr/local/bin/node test config/nightly.yaml --incognito --filename nightly-private --except=duckduckgo >> nightly-private-log.txt
-/usr/local/bin/node test config/android.yaml >> android-log.txt
-/usr/local/bin/node test config/iOS.yaml >> ios-log.txt
+#/usr/local/bin/node test config/nightly.yaml >> nightly-log.txt
+#/usr/local/bin/node test config/nightly.yaml --incognito --filename nightly-private --except=duckduckgo >> nightly-private-log.txt
+#/usr/local/bin/node test config/android.yaml >> android-log.txt
+#/usr/local/bin/node test config/iOS.yaml >> ios-log.txt
 #/usr/local/bin/node test config/android-nightly.yaml >> android-nightly-log.txt
 
