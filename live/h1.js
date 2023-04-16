0cfd6e6a39d0dacb4302d1cbd1fe273a15635b59
Arthur Edelstein <arthuredelstein@gmail.com>
2023-04-16 13:15:25 -0700

    semistandard fixes to h1.js

--- live/h1.js
+++ live/h1.js
@@ -1,5 +1,5 @@
 const https = require('https');
-const url = require("url");
+const url = require('url');
 const fs = require('fs');
 
 const options = {
@@ -7,7 +7,7 @@ const options = {
   cert: fs.readFileSync('/etc/letsencrypt/live/h1.privacytests2.org/fullchain.pem')
 };
 
-let socketTags = new Map();
+const socketTags = new Map();
 
 // Create a secure HTTP1 server
 // Examples:
@@ -15,21 +15,21 @@ let socketTags = new Map();
 // https://h1.privacytests2.org:8901/?mode=read
 const server = https.createServer(options, (request, response) => {
   console.log(request.url);
-  let parsedURL = url.parse(request.url, true);
-  let query = parsedURL.query;
-  let socket = request.socket;
-  //console.log(socket);
+  const parsedURL = new url.URL(request.url, true);
+  const query = parsedURL.query;
+  const socket = request.socket;
+  // console.log(socket);
   response.setHeader('Content-Type', 'text/plain; charset=utf-8');
   response.setHeader('Cache-Control', 'no-store');
   response.setHeader('Access-Control-Allow-Origin', '*');
   response.setHeader('Connection', 'Keep-Alive');
   response.setHeader('Keep-Alive', 'timeout=300, max=1000');
-  if (query["mode"] === "write") {
-    socketTags.set(socket, query["secret"]);
-    response.end("empty");
-  } else if (query["mode"] === "read") {
-    let tagFound = socketTags.get(socket);
-    console.log("h1 read request. socket tag found:", tagFound);
+  if (query.mode === 'write') {
+    socketTags.set(socket, query.secret);
+    response.end('empty');
+  } else if (query.mode === 'read') {
+    const tagFound = socketTags.get(socket);
+    console.log('h1 read request. socket tag found:', tagFound);
     response.end(tagFound);
   } else {
     response.end();
@@ -37,7 +37,7 @@ const server = https.createServer(options, (request, response) => {
 });
 
 server.keepAliveTimeout = 300000;
-console.log("server.keepAliveTimeout:", server.keepAliveTimeout);
+console.log('server.keepAliveTimeout:', server.keepAliveTimeout);
 
 server.listen(8901);
-console.log("listening for h1 connections on 8901");
+console.log('listening for h1 connections on 8901');
