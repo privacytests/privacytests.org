6d22d4f30ac274f1493bd568238b4b47b75b04b7
Arthur Edelstein <arthuredelstein@gmail.com>
2023-04-16 11:53:31 -0700

    semistandard fixes to utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -2,25 +2,25 @@
 
 const fs = require('fs');
 const YAML = require('yaml');
-const child_process = require('child_process');
+const childProcess = require('child_process');
 const datauri = require('datauri/sync');
 
 const sleepMs = (t) => new Promise((resolve, reject) => setTimeout(resolve, t));
 
 const execSync = (command, options) => {
   console.log(command);
-  return child_process.execSync(command, options);
+  return childProcess.execSync(command, options);
 };
 
 const exec = (command, options) => {
   console.log(command);
-  return child_process.exec(command, options);
+  return childProcess.exec(command, options);
 };
 
 // Returns an list of the PIDs for immediate children
 const childProcesses = (pid) => {
   try {
-    const list = child_process.execSync(`pgrep -P ${pid}`).toString();
+    const list = childProcess.execSync(`pgrep -P ${pid}`).toString();
     return list.split(/\s+/).filter(s => s.length > 0);
   } catch (e) {
     return [];
@@ -31,13 +31,13 @@ const childProcesses = (pid) => {
 // the original PID itself.
 const descendantProcesses = (pid) => {
   const children = childProcesses(pid);
-  descendants = children.map(descendantProcesses).flat();
+  const descendants = children.map(descendantProcesses).flat();
   return [pid, ...descendants];
 };
 
 // Kill all processes in the list of PIDs.
 const killProcesses = (pids) => {
-  pids.map(pid => {
+  pids.forEach(pid => {
     try {
       process.kill(pid, process.SIGTERM);
     } catch (e) {
@@ -57,6 +57,6 @@ const readYAMLFile = (file) => {
   return YAML.parse(fileContents);
 };
 
-const dataUriFromFile = path => datauri(`${__dirname}/${path}`).content;
+const dataUriFromFile = path => datauri(path.join(__dirname, path)).content;
 
 module.exports = { sleepMs, execSync, exec, killProcessAndDescendants, readYAMLFile, dataUriFromFile };
