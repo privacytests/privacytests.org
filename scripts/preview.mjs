ec0bbd236a0855bcc0f839fdf45d95bc6da41c66
Arthur Edelstein <arthuredelstein@gmail.com>
2023-04-16 11:59:50 -0700

    semistandard fixes to preview.mjs

--- scripts/preview.mjs
+++ scripts/preview.mjs
@@ -1,12 +1,13 @@
 import captureWebsite from 'capture-website';
 
 const createPreviewImage = async (htmlFile, pngFile) => {
-    await captureWebsite.file(htmlFile, pngFile, {
-      width: 1000,
-      height: 500, scaleFactor: 1.2,
-      overwrite: true
-    });
-    console.log("wrote", pngFile);
-}
+  await captureWebsite.file(htmlFile, pngFile, {
+    width: 1000,
+    height: 500,
+    scaleFactor: 1.2,
+    overwrite: true
+  });
+  console.log('wrote', pngFile);
+};
 
 export { createPreviewImage };
