cf2398909524ef7b08914273afddd61b9971213f
Arthur Edelstein <arthuredelstein@gmail.com>
2023-04-16 13:16:48 -0700

    semistandard fixes to tls.js

--- live/tls.js
+++ live/tls.js
@@ -1,13 +1,12 @@
 const https = require('https');
 const fs = require('fs');
 
-
 const START_SESSION_ID = 16;
 const START_MASTER_KEY = 50;
 
-let parseSession = (buf) => ({
-  sessionId: buf.slice(START_SESSION_ID, START_SESSION_ID+32).toString('hex'),
-  masterKey: buf.slice(START_MASTER_KEY, START_MASTER_KEY+48).toString('hex')
+const parseSession = (buf) => ({
+  sessionId: buf.slice(START_SESSION_ID, START_SESSION_ID + 32).toString('hex'),
+  masterKey: buf.slice(START_MASTER_KEY, START_MASTER_KEY + 48).toString('hex')
 });
 
 const options = {
@@ -15,36 +14,38 @@ const options = {
   cert: fs.readFileSync('/etc/letsencrypt/live/tls.privacytests2.org/fullchain.pem')
 };
 
-let lastRequest = null;
-
-let theServer = https.createServer(options, function (req, res) {
-  let result = { isSessionReused: req.socket.isSessionReused(),
-                 sessionId: parseSession(req.socket.getSession()).sessionId };
-  res.writeHead(200, { 'Content-Type': 'application/json',
-                       'Cache-Control': 'no-store',
-                       'Access-Control-Allow-Origin': '*'});
+const theServer = https.createServer(options, function (req, res) {
+  const result = {
+    isSessionReused: req.socket.isSessionReused(),
+    sessionId: parseSession(req.socket.getSession()).sessionId
+  };
+  res.writeHead(200, {
+    'Content-Type': 'application/json',
+    'Cache-Control': 'no-store',
+    'Access-Control-Allow-Origin': '*'
+  });
   res.end(JSON.stringify(result));
-  console.log("response sent");
+  console.log('response sent');
   req.socket.destroy();
-//  console.log(req);
-  lastRequest = req;
 });
 
-console.log("eventNames", theServer.eventNames());
+console.log('eventNames', theServer.eventNames());
 
 theServer.on('keylog', (line, tlsSocket) => {
 //  console.log(line.toString());
 });
 
 theServer.on('newSession', (sessionId, sessionData, callback) => {
-  let results = { sessionId: sessionId.toString("hex"),
-                  sessionData: sessionData.toString("hex")};
-//  console.log(results);
+  const results = {
+    sessionId: sessionId.toString('hex'),
+    sessionData: sessionData.toString('hex')
+  };
+  console.log(results);
   callback();
 });
 
 theServer.on('secureConnection', (tlsSocket) => {
-  let session = tlsSocket.getSession();
+  const session = tlsSocket.getSession();
   console.log(tlsSocket.isSessionReused(), parseSession(session));
 //  console.log(buffer, buffer.length);
 //  console.log(tlsSocket.getTLSTicket());
@@ -52,4 +53,4 @@ theServer.on('secureConnection', (tlsSocket) => {
 
 theServer.keepAliveTimeout = 300000;
 theServer.listen(8900);
-console.log("listening for tls connections on 8900");
+console.log('listening for tls connections on 8900');
