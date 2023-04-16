effc3b93dcc16f20584f86c02a0b62cca46d203f
Arthur Edelstein <arthuredelstein@gmail.com>
2023-04-16 11:56:54 -0700

    semistandard fixes to system-proxy.js

--- scripts/system-proxy.js
+++ scripts/system-proxy.js
@@ -1,5 +1,6 @@
 const { execSync } = require('child_process');
 
+/*
 const exampleOutput = `
 An asterisk (*) denotes that a network service is disabled.
 (1) Ethernet
@@ -11,52 +12,53 @@ An asterisk (*) denotes that a network service is disabled.
 (3) Wi-Fi
 (Hardware Port: Wi-Fi, Device: en1)
 `;
+*/
 
 const run = (...args) => execSync(...args).toString();
 
 const getPreferredDevice = () => {
-  const result = run("/sbin/route get example.com | /usr/bin/grep interface");
-  return result.split(":")[1].trim();
+  const result = run('/sbin/route get example.com | /usr/bin/grep interface');
+  return result.split(':')[1].trim();
 };
 
 const parseNetworkServiceList = (text) => {
-  const groups = text.split("\n\n");
-  let result = {};
-  for (let group of groups) {
-    let lines = group.split("\n");
+  const groups = text.split('\n\n');
+  const result = {};
+  for (const group of groups) {
+    const lines = group.split('\n');
     let hardwarePort, device;
-    for (let line of lines) {
-      const chunks =line.split(": ");
-      if (chunks[0] === "Hardware Port") {
+    for (const line of lines) {
+      const chunks = line.split(': ');
+      if (chunks[0] === 'Hardware Port') {
         hardwarePort = chunks[1];
-      } else if (chunks[0] === "Device") {
+      } else if (chunks[0] === 'Device') {
         device = chunks[1];
       }
     }
     if (device && hardwarePort) {
       result[device] = hardwarePort;
     }
-}
+  }
   return result;
 };
 
 const getNetworkServices = () => {
-  const result = run("/usr/sbin/networksetup -listallhardwareports");
+  const result = run('/usr/sbin/networksetup -listallhardwareports');
   return parseNetworkServiceList(result);
 };
 
 const getPreferredNetworkService = () => getNetworkServices()[getPreferredDevice()];
 
 const setProxyState = (networkService, type, enabled) => {
-  const state = enabled ? "on" : "off";
+  const state = enabled ? 'on' : 'off';
   return run(`/usr/sbin/networksetup -set${type}proxystate "${networkService}" "${state}"`);
 };
 
 const setProxy = (networkService, type, { enabled, domain, port, authenticated, username, password }) => {
-  //console.log(networkService, type, { enabled, domain, port, authenticated, username, password });
-  let authenticatedString = authenticated === undefined ? "" : (authenticated ? "on" : "off");
-  let usernameString = username === undefined ? "" : username;
-  let passwordString = password === undefined ? "" : password;
+  // console.log(networkService, type, { enabled, domain, port, authenticated, username, password });
+  const authenticatedString = authenticated === undefined ? '' : (authenticated ? 'on' : 'off');
+  const usernameString = username === undefined ? '' : username;
+  const passwordString = password === undefined ? '' : password;
   if (domain !== undefined && port !== undefined) {
     let command = `/usr/sbin/networksetup -set${type}proxy "${networkService}" "${domain}" "${port}"`;
     if (authenticatedString) {
@@ -70,21 +72,21 @@ const setProxy = (networkService, type, { enabled, domain, port, authenticated, 
 };
 
 const parseGetterResult = raw => {
-  const lines = raw.split("\n");
-  let rawMap = {};
+  const lines = raw.split('\n');
+  const rawMap = {};
   for (const line of lines) {
-    const [key, val] = line.split(":").map(s => s.trim());
+    const [key, val] = line.split(':').map(s => s.trim());
     if (key.length > 0) {
       rawMap[key] = val;
     }
   }
-  let result = {};
-  result["enabled"] = rawMap["Enabled"] === 'Yes';
-  const server = rawMap["Server"];
-  result["domain"] = server === "0" ? undefined : server;
-  const port = rawMap["Port"];
-  result["port"] = port === "0" ? undefined : port;
-  result["authenticated"] = { "0": false, "1": true }[rawMap["Authenticated Proxy Enabled"]];
+  const result = {};
+  result.enabled = rawMap.Enabled === 'Yes';
+  const server = rawMap.Server;
+  result.domain = server === '0' ? undefined : server;
+  const port = rawMap.Port;
+  result.port = port === '0' ? undefined : port;
+  result.authenticated = { 0: false, 1: true }[rawMap['Authenticated Proxy Enabled']];
   return result;
 };
 
@@ -94,15 +96,15 @@ const getProxy = (networkService, type) => {
 };
 
 const setProxies = (networkService, settings) => {
-  for (let type in settings) {
+  for (const type in settings) {
     setProxy(networkService, type, settings[type]);
   }
 };
 
 const getProxies = (networkService) => {
-  const types = ["ftp", "web", "secureweb", "streaming", "gopher", "socksfirewall"];
-  let result = {};
-  for (let type of types) {
+  const types = ['ftp', 'web', 'secureweb', 'streaming', 'gopher', 'socksfirewall'];
+  const result = {};
+  for (const type of types) {
     result[type] = getProxy(networkService, type);
   }
   return result;
@@ -114,8 +116,8 @@ const runTests = () => {
   const oldSettings = getProxies(networkService);
   console.log(oldSettings);
   setProxies(networkService, {
-    "web": { enabled: true, domain: "127.0.0.1", port: 8080 },
-    "secureweb": { enabled: true, domain: "127.0.0.1", port: 8080 }
+    web: { enabled: true, domain: '127.0.0.1', port: 8080 },
+    secureweb: { enabled: true, domain: '127.0.0.1', port: 8080 }
   });
   console.log(getProxies(networkService));
   setTimeout(() => {
