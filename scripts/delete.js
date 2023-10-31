90f9d3921c3bcd26969a97426b6377291b75ffe7
Arthur Edelstein <arthuredelstein@gmail.com>
2023-10-31 12:25:10 -0700

    clean up delete.js

--- scripts/delete.js
+++ scripts/delete.js
@@ -1,14 +1,18 @@
 const fs = require('fs');
 
+const readJsonFile = (inputFile) => JSON.parse(fs.readFileSync(inputFile).toString());
+
 // Main program. Usage:
 // `node delete out/results/20220422/nightly.json brave 1.39`
 const main = () => {
   const [inputFile, indexString] = process.argv.slice(2);
-  const inputObject = JSON.parse(fs.readFileSync(inputFile).toString());
+  const inputObject = readJsonFile(inputFile);
   delete inputObject.all_tests[parseInt(indexString)];
   inputObject.all_tests = inputObject.all_tests.filter(test => test);
   fs.copyFileSync(inputFile, inputFile + '.original');
   fs.writeFileSync(inputFile, JSON.stringify(inputObject));
 };
 
-main();
+if (require.main === module) {
+  main();
+}
