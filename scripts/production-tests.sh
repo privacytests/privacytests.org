4aa82aa3293fcb89c41aa18a725a49b891901732
Arthur Edelstein <arthuredelstein@gmail.com>
2024-11-04 08:05:17 -0800

    no need for separate safari step anymore

--- scripts/production-tests.sh
+++ scripts/production-tests.sh
@@ -1,12 +1,10 @@
 /usr/local/bin/node test config/desktop.yaml --update
 /usr/local/bin/node test config/nightly.yaml --update
 /usr/local/bin/node test config/desktop.yaml
-/usr/local/bin/node test config/desktop.yaml --except=safari --incognito --filename private
-/usr/local/bin/node test --browser=safari --incognito --filename=private-safari
+/usr/local/bin/node test config/desktop.yaml --incognito --filename private
 /usr/local/bin/node test --browser=brave --tor --repeat 5 --filename=brave-tor
 /usr/local/bin/node test config/nightly.yaml
-/usr/local/bin/node test config/nightly.yaml --incognito --filename nightly-private --except=safari,duckduckgo
-/usr/local/bin/node test --browser=safari --incognito --nightly --filename=nightly-private-safari
+/usr/local/bin/node test config/nightly.yaml --incognito --filename nightly-private --except=duckduckgo
 /usr/local/bin/node test --browser=brave --tor --repeat 5 --filename=brave-tor-nightly --nightly
 /usr/local/bin/node test config/android.yaml
 /usr/local/bin/node test config/iOS.yaml
